2606aa5caf3537985b32df7d0ba676dd8460404a
Ben Perlmutter <benperlmutter94@gmail.com>
2020-08-04 19:55:26 -0500

    add getLanguageQuestions fxn

--- src/state-management/getLanguageQuestions.js
+++ src/state-management/getLanguageQuestions.js
@@ -1,6 +1,14 @@
-
-export default function getLanguageQuestions(lang){
+import { getActiveLangName, getActiveLangQuestions } from "./localStorageFns";
+export default async function getLanguageQuestions(lang){
     //check if language is in local storage
-    //if in LS, set in SL 
-    //if nothing in LS, fetch from API
+    if(getActiveLangName() === lang) return getActiveLangQuestions();
+    
+    const res = await fetch(`https://api.javascript-trivia.com/${lang}/`);
+    if(res.ok){
+        const {questions} = await res.json();
+        setActiveLangQuestions(questions);
+        return questions;
+    } else{
+        return ({"error": "There was an error getting the questions. Please try again later."})
+    }
 }
