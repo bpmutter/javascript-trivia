494aa74db26d24ed14e8bab9518602d4bf58f225
Ben Perlmutter <benperlmutter94@gmail.com>
2020-08-04 20:18:55 -0500

    add basic game structure

---
+++ src/components/GameNavigation.js
@@ -0,0 +1,5 @@
+import React, { useState, useContext } from "react";
+
+export default function GameWithContext() {
+  return <p>derp derp derp</p>;
+}
---
+++ src/components/GameWithContext.js
@@ -0,0 +1,15 @@
+//notes on game:
+/* 
+so the game will basically be a full client-side react app with state management and data persistence occurring in the browser
+
+so at this top "app" lvl, need to set context, and then there are basically 2 parallel systems running to persist data, context that the app is doing, and then a mirror image of it is being passed to localstorage for when the user comes back to the website.
+
+*/
+import React, {useState, useContext} from 'react';
+
+export default function GameWithContext(){
+
+    return(<p>
+        derp derp derp
+    </p>)
+}
